265d287005ef6ae61d590944e1f7c06fadd4f269
GaryLeeson <gary@garyleeson.com>
2018-06-07 19:56:04 +0100

    1.1.0 Adding Consul Support

--- usecases/registry.go
+++ usecases/registry.go
@@ -1,23 +1,33 @@
 package usecases
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/riomhaire/figura/frameworks/serviceregistry"
+)
 
 // Configuration containing data from the environment which is used to define program behaviour
 type Configuration struct {
 	Application           string
 	Version               string
 	Profiling             bool
+	Host                  string
 	Port                  int
 	ConfigurationLocation string
+
+	Consul     bool
+	ConsulHost string
+	ConsulId   string // ID of this client
 }
 
 type Registry struct {
-	Configuration        Configuration
-	Logger               Logger
-	ConfigurationReader  ConfigurationInteractor
-	ConfigurationStorage ConfigurationStorage
-	Storage              Storage
-	Authenticator        TokenInteractor
+	Configuration           Configuration
+	Logger                  Logger
+	ConfigurationReader     ConfigurationInteractor
+	ConfigurationStorage    ConfigurationStorage
+	Storage                 Storage
+	Authenticator           TokenInteractor
+	ExternalServiceRegistry serviceregistry.ServiceRegistry
 }
 
 func (c *Configuration) String() string {
